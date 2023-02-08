749fd43417ecfb9bda8c6e39d8e78b661d1c4e20
Rodrigo Rodriguez <gothenk@Rodrigos-iMac.local>
2023-02-08 10:50:07 -0500

    First logger-service commit

---
+++ cmd/api/handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log-service/data"
+	"net/http"
+)
+
+type JSONPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	//read json into var
+	var requesPayload JSONPayload
+	_ = app.readJSON(w, r, &requesPayload)
+
+	// insert data
+	event := data.LogEntry{
+		Name: requesPayload.Name,
+		Data: requesPayload.Data,
+	}
+
+	err := app.Models.LogEntry.Insert(event)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJSON(w, http.StatusAccepted, resp)
+}
